70343b6dfecb9e1da707d6adf974e4fe45c29bd1
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-11-05 17:02:47 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,1 +1,13 @@
-body style="background-color: #FF0000;"
+
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #f0f0f5;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
+st.title("Mi aplicación Streamlit")
+st.write("Este es un ejemplo con fondo personalizado.")
