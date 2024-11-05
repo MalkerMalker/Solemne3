12f8c217ed12eb2e95e2edb63272516e507db80f
Diego <diegoabva2003@gmail.com>
2024-11-05 17:11:21 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,3 +1,8 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import altair as alt
+
 st.title("Mi aplicación Streamlit")
 st.write("Este es un ejemplo con fondo personalizado.")
