24092776596159eed4442ef89eb148f77a82d687
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-11-05 16:58:29 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,4 +1,4 @@
-backgroundColor = "#f0f0f0"  # Color de fondo (puedes cambiar el color)
+backgroundColor = "#000000"  # Color de fondo (puedes cambiar el color)
 primaryColor = "#FF6347"  # Color principal
 secondaryBackgroundColor = "#ffffff"  # Color de fondo de los widgets
 textColor = "#333333"  # Color del texto
