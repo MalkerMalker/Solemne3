b77fe648a0f6bda54ee974d85886e92419e51a54
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-11-05 16:51:52 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,14 +2,10 @@ import streamlit as st
 import webbrowser
 import pandas as pd
 import numpy as np
-# Cambiar el fondo de la página con CSS
 st.markdown("""
     <style>
-        body {
-            background-color: #f0f0f0;  # Color de fondo de la página
-        }
-        .stApp {
-            background-color: #f0f0f0;  # Fondo de la aplicación
+        .css-1d391kg {
+            background-color: #ADD8E6;  # Color de fondo de la barra lateral
         }
     </style>
 """, unsafe_allow_html=True)
