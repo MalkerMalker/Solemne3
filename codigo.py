d97ccdf8d7262172e98627f1916e935394d78d2e
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-11-05 16:55:25 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,9 +2,3 @@ import streamlit as st
 import webbrowser
 import pandas as pd
 import numpy as np
-[theme]
-primaryColor="#FF4B4B"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#31333F"
-font="sans serif"
