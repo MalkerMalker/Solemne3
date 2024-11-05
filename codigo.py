cf2c15d522dd49d2adab3f6e50c28d9be96e1412
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-11-05 16:48:24 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -4,3 +4,17 @@ import pandas as pd
 import numpy as np
 
 st.title("op.gg")
+import streamlit as st
+
+# Cambiar el fondo y el color del texto con CSS
+st.markdown("""
+    <style>
+        body {
+            background-color: #f0f0f0;
+            color: #333333;
+        }
+        .stApp {
+            background-color: #f0f0f0;
+        }
+    </style>
+""", unsafe_allow_html=True)
