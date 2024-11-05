50850075e177d6721e13fc0a605564b12c00cc12
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-11-05 17:08:55 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,14 +1,4 @@
 import streamlit as st
-
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: #000000;
-    }
-    </style>
-    """, 
-    unsafe_allow_html=True
-)
+st.markdown()
 st.title("Mi aplicación Streamlit")
 st.write("Este es un ejemplo con fondo personalizado.")
