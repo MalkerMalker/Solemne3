f827d614ea088c75119716790d440c00c6d67eca
Diego <diegoabva2003@gmail.com>
2024-11-05 17:12:24 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -4,5 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import altair as alt
 
+st.sidebar.image("imagenes/logo.png")
 st.title("Mi aplicación Streamlit")
 st.write("Este es un ejemplo con fondo personalizado.")
