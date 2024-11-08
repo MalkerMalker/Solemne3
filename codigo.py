77bf09ccd62bef9537f6be47050e984574b2fcbc
Diego <diegoabva2003@gmail.com>
2024-11-08 11:50:24 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,30 +2,17 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
-import base64
 
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"] {
+background-image: url("https://www.wallpaperbetter.com/wallpaper/806/22/557/league-of-legends-landscape-magic-hd-1080P-wallpaper.jpg")
+background-size: cover;
+}
+</style>
+"""
 
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-
-
-set_background('imagenes/fondo.png')
-
-
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.image("imagenes/letras.png")
 st.sidebar.image("imagenes/logo.png")
