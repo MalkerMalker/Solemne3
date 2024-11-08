848ee94b7d4e30948f19af3098ec87e2b3562585
Diego <diegoabva2003@gmail.com>
2024-11-08 12:07:48 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -4,16 +4,16 @@ import pandas as pd
 import altair as alt
 import base64
 
+
+#fondo
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
     return base64.b64encode(data).decode()
 
-# Ruta de tu imagen de fondo
 bg_image_path = "imagenes/fondo.jpg"
 bg_image_base64 = get_base64_of_bin_file(bg_image_path)
 
-# CSS para el fondo
 page_bg_img = f"""
 <style>
     .stApp {{
@@ -23,9 +23,10 @@ page_bg_img = f"""
     }}
 </style>
 """
-
-# Insertar CSS
 st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
 
 st.image("imagenes/letras.png")
 st.sidebar.image("imagenes/logo.png")
