667cd48e8fad06fc1810bacaf8c6f16d732de171
Diego <diegoabva2003@gmail.com>
2024-11-08 11:11:07 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,6 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
+
+[theme] backgroundColor = "#F0F0F0"
 st.image("imagenes/letras.png")
 st.sidebar.image("imagenes/logo.png")
 st.title("League Of Legends")
