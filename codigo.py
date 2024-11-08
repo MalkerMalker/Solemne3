c3ecef25445a8505a42782c55d67a829ae9c3d74
Diego <diegoabva2003@gmail.com>
2024-11-08 11:17:15 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -7,13 +7,3 @@ st.image("imagenes/letras.png")
 st.sidebar.image("imagenes/logo.png")
 st.title("League Of Legends")
 st.write(".")
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: url("https://www.wallpaperbetter.com/wallpaper/806/22/557/league-of-legends-landscape-magic-hd-1080P-wallpaper.jpg");
-    }
-   </style>
-    """,
-    unsafe_allow_html=True
-)
