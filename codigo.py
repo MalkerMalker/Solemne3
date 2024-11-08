c610c00d9f0f61fda3da7f2f0351e507e2e9bf58
Diego <diegoabva2003@gmail.com>
2024-11-08 11:17:32 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -3,6 +3,18 @@ import numpy as np
 import pandas as pd
 import altair as alt
 
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://www.wallpaperbetter.com/wallpaper/806/22/557/league-of-legends-landscape-magic-hd-1080P-wallpaper.jpg");
+    }
+   </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
 st.image("imagenes/letras.png")
 st.sidebar.image("imagenes/logo.png")
 st.title("League Of Legends")
