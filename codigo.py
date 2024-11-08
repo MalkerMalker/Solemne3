9e0193ba7ee351ab79ca546a740aa8dbbb078a67
Diego <diegoabva2003@gmail.com>
2024-11-08 11:32:50 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -3,6 +3,27 @@ import numpy as np
 import pandas as pd
 import altair as alt
 
+# Ruta a la imagen local
+image_path = "imagenes/fondo.jpg"
+
+# Usar st.markdown para insertar el estilo CSS y establecer la imagen de fondo
+st.markdown(
+    f"""
+    <style>
+    body {{
+        background: url("{image_path}");
+        background-size: cover;  /* Hace que la imagen cubra toda la pantalla */
+        background-position: center;  /* Centra la imagen */
+        background-attachment: fixed;  /* Mantiene el fondo fijo mientras haces scroll */
+    }}
+    .reportview-container {{
+        background: transparent;  /* Asegura que no haya un fondo blanco en el contenedor */
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 
 st.image("imagenes/letras.png")
 st.sidebar.image("imagenes/logo.png")
