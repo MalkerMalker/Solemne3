9bd8f476dec2061e4a3d0e7665cad6ac5b697f78
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-11-12 15:12:49 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -43,6 +43,8 @@ elif opcion == 'Competitivo':
     st.markdown("<h1 style='color: white;'>Competitivo</h1>", unsafe_allow_html=True)
     video_url = "https://www.youtube.com/watch?v=Kv2rswmxBVs"
     st.video(video_url)
+    <img id="zoom_04" src="small/image1.png"
+     data-zoom-image="large/image1.jpg"/>
 elif opcion == 'Acerca de':
     st.write('Aquí se mostraría la información adicional.')
 
