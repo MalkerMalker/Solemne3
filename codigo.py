53c89fb8106bb77a701f9ea88653043cfecdf467
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-11-19 15:51:05 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -180,7 +180,12 @@ if opcion == 'Información':
 elif opcion == 'Campeones':
     col1, col2, col3, col4, col5, col6, col7, col8, col9, col10 = st.columns(10)
     with col1:
-        st.image("imagenes/eclipseinf.jpg")
+        st.markdown("""
+<a href="https://www.youtube.com/watch?v=aR-KAldshAE" target="_blank">
+    <img src="imagenes/eclipseinf.jpg" alt="Imagen de fondo" style="width:100%; height:auto; cursor: pointer;">
+</a>
+""", unsafe_allow_html=True)
+        #st.image("imagenes/eclipseinf.jpg")
     with col2:
         st.image("imagenes/ludencompanioninf.jpg")
     with col3:
