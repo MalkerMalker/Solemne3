4a16d7629d693bcb0fde4a29c4707edcd4f3a474
Diego <diegoabva2003@gmail.com>
2024-11-19 15:02:33 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -168,25 +168,25 @@ if opcion == 'Información':
     
 elif opcion == 'Campeones':
     col1, col2, col3, col4, col5, col6, col7, col8, col9, col10 = st.columns(10)
-    #with col1:
+    with col1:
         st.image("imagenes/eclipseinf.jpg")
-    #with col2:
+    with col2:
         st.image("imagenes/ludencompanioninf.jpg")
-    #with col3:
+    with col3:
         st.image("imagenes/eclipseinf.jpg")
-    #with col4:
+    with col4:
         st.image("imagenes/ludencompanioninf.jpg")
-    #with col5:
+    with col5:
         st.image("imagenes/eclipseinf.jpg")
-    #with col6:
+    with col6:
         st.image("imagenes/ludencompanioninf.jpg")
-    #with col7:
+    with col7:
         st.image("imagenes/eclipseinf.jpg")
-    #with col8:
+    with col8:
         st.image("imagenes/ludencompanioninf.jpg")
-    #with col9:
+    with col9:
         st.image("imagenes/eclipseinf.jpg")
-    #with col10:
+    with col10:
         st.image("imagenes/ludencompanioninf.jpg")
 
 
