69a07d6f45927aa2fb74d86308b76e1949d45c08
Diego <diegoabva2003@gmail.com>
2024-11-20 19:04:03 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -497,7 +497,7 @@ elif opcion == 'Campeones':
             with icon_col1:
                 st.image(tagimagen, width=30)
                 st.image(rolimagen, width=30)
-                st.image(rangeimagen, width=30)
+                st.image(rangeimagen, width=25)
             with texto:
                 st.write(tagtexto)
                 st.write(roltexto)
