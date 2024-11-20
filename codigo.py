3c989a87f21ba16cb56ed7f6c861e942cf8fd1c8
Diego <diegoabva2003@gmail.com>
2024-11-20 16:47:13 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -399,7 +399,7 @@ elif opcion == 'Campeones':
         elif tag2 == "Support":
             tagimagen2 = "imagenes/assets/support.png"
             tagtexto2 = "Soporte"
-        elif tag2 == "":
+        elif tag2 == None:
             tagimagen2 = "imagenes/assets/invisible.png"
             tagtexto2 = ""
         
