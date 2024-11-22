b206dc917ea782fd8b3b9d819c055b942082052d
Diego <diegoabva2003@gmail.com>
2024-11-22 18:49:46 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -540,6 +540,7 @@ elif opcion == 'Campeones':
                 st.write("Rango:")
                 st.write("Recurso:")
                 st.write("HP/HP+:")
+                st.write("Mana/Mana+:")
             with icon_col1:
                 st.image(tagimagen, width=30)
                 st.image(rolimagen, width=30)
