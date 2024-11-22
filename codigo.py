862da583c9fbdfb1d01a5f8c4206e78b2fd60e41
Diego <diegoabva2003@gmail.com>
2024-11-22 20:22:16 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -578,7 +578,7 @@ elif opcion == 'Campeones':
                 st.image("imagenes/assets/saludpornivel.png",width=30)
                 st.image("imagenes/assets/manapornivel.png", width=30)
                 st.image("imagenes/assets/invisible.png",width=30)
-                st.image("imagenes/assets/armadurapornivel.png",width=20)
+                st.image("imagenes/assets/armadurabase.png", width=30)
             with texto2:
                 st.write(tagtexto2)  
                 st.write(roltexto2)
