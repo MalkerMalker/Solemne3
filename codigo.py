058911fca5406ad62be77af2ba16164b4937000d
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-11-22 11:51:13 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -452,7 +452,13 @@ elif opcion == 'Campeones':
 
         rtype = dfchamp.loc[dfchamp["Name"] == mensaje, "Resourse type"].values[0]
         if rtype == "Blood Well":
-            imagensangre = "imagenes/assets/melee.png"
+            rtypeimagen = "imagenes/assets/gotasangre.png"
+            rtypetexto = "Blood Well"
+            
+        elif range == "Ranged":
+            rangeimagen = "imagenes/assets/rango.png"
+            rangetexto = "Distancia"
+
         
         rutaimagen = dfchamp.loc[dfchamp["Name"] == mensaje, "Icono"].values
         rutasplash = dfchamp.loc[dfchamp["Name"] == mensaje, "SplashArt"].values
