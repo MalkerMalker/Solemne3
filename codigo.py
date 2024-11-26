3bb55529ab8e23601d69ead387b3a311372c69c3
Diego <diegoabva2003@gmail.com>
2024-11-25 22:41:13 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -830,7 +830,97 @@ elif opcion == 'Campeones':
 
 
 elif opcion == 'Competitivo':
-    st.write("wenaboki")
+    st.markdown("<h1 style='color: white;'>Competitivo</h1>", unsafe_allow_html=True)
+    video_url = "https://www.youtube.com/watch?v=Kv2rswmxBVs"
+    st.video(video_url)
+    st.markdown(
+    """
+    <div style="margin-left: -75px; margin-right: -75px;">
+        <h3 style="color: white;">¿Que es el competitivo de league of legends?</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>El competitivo de League of Legends (LoL) se refiere a los torneos y ligas organizados por Riot Games donde equipos profesionales compiten por premios y reconocimiento. Estos torneos incluyen eventos regionales, nacionales e internacionales, como la League of Legends Championship Series (LCS), la League of Legends European Championship (LEC), y el Campeonato Mundial de League of Legends (Worlds), todo esto se realiza en amplios lugares para que asi los fanaticos de este juego puedan asistir de forma presencial y vivir una experiencia inolvidable.
+    </div>
+    """, unsafe_allow_html=True)
+    
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>Los Worlds de League of Legends son uno de los eventos de esports más conocidos debido a su audiencia global enorme, alta competitividad, prestigio histórico desde 2011, impresionante producción, participación de equipos icónicos como T1 y G2 Esports, accesibilidad gratuita en múltiples plataformas de streaming y el involucramiento activo de la comunidad con eventos adicionales y contenido exclusivo. Estos factores combinados hacen que sean uno de los eventos más esperados y seguidos en el mundo de los esports. En donde el ganador podra hacerse con la increible copa.
+    </div>
+    """, unsafe_allow_html=True)
+    st.image("Copa Worlds.jpeg")
+
+    st.markdown(
+    """
+    <div style="margin-left: -75px; margin-right: -75px;">
+        <h3 style="color: white;">Rangos Competitivos</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+    
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>Los rangos competitivos en League of Legends son una forma de clasificar a los jugadores según su habilidad y rendimiento en el juego. Estos rangos ayudan a emparejar a los jugadores con oponentes de habilidad similar para garantizar partidas justas y equilibradas. Aquí están los rangos competitivos de League of Legends, de menor a mayor:
+    </div>
+    """, unsafe_allow_html=True)
+    st.image("Rangos Competitivos.png")
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>Bronce es para jugadores que tienen un entendimiento básico del juego pero aún están desarrollando sus habilidades y estrategias. Plata es para jugadores promedio que tienen una buena comprensión del juego pero que todavía están trabajando en perfeccionar sus habilidades y tácticas. Oro es para jugadores por encima del promedio, con una comprensión sólida del juego y habilidades decentes. Este es un rango donde los jugadores comienzan a dominar las mecánicas básicas del juego. Platino es para jugadores habilidosos con una comprensión avanzada del juego y estrategias más refinadas. Aquí es donde los jugadores muestran un alto nivel de competencia. Esmeralda es un rango intermedio entre Platino y Diamante, reservado para jugadores que destacan pero no alcanzan el nivel de los mejores. Diamante es para jugadores altamente habilidosos, considerados entre los mejores del servidor. Estos jugadores tienen un gran conocimiento del juego y habilidades sobresalientes. Maestro es para jugadores de élite que están en la cima del competitivo. Tienen una comprensión profunda del juego y habilidades excepcionales. Gran Maestro es para jugadores que están justo debajo del nivel más alto, mostrando un nivel de competencia extremadamente alto y un dominio casi completo del juego. Challenger es para los mejores jugadores del servidor, en la cúspide del competitivo. Estos jugadores representan la élite absoluta y compiten al más alto nivel. Estos rangos ayudan a emparejar a los jugadores con oponentes de habilidad similar, creando partidas más justas y desafiantes. Cada rango está dividido en divisiones, excepto Maestro, Gran Maestro y Challenger, que no tienen divisiones
+    </div>
+    """, unsafe_allow_html=True)
+    st.image("Tiers.jpg")
+
+    st.markdown(
+    """
+    <div style="margin-left: -75px; margin-right: -75px;">
+        <h3 style="color: white;">Videos</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+    video_url = "https://www.youtube.com/watch?v=XSe08RXp1P8"
+    st.video(video_url)
+    st.markdown(
+    """
+    <div style="margin-left: -75px; margin-right: -75px;">
+        <h3 style="color: white;">El mejor jugador de todos los tiempos</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>Lee "Faker" Sang-hyeok es universalmente aclamado como el mejor jugador de League of Legends de todos los tiempos. Conocido como el "Rey Demonio Indomable", Faker ha dejado una marca indeleble en el competitivo de esports gracias a su destreza sin igual, su capacidad para realizar jugadas decisivas en momentos cruciales y su impresionante consistencia a lo largo de su carrera. Además de su éxito en torneos, Faker es conocido por su humildad y dedicación. Ha sido una fuente constante de inspiración para jugadores de todo el mundo, demostrando que el verdadero talento viene acompañado de una ética de trabajo inquebrantable y un deseo interminable de mejorar.El legado de Faker no solo se mide en títulos y premios, sino en el impacto duradero que ha tenido en la comunidad de esports. Ha elevado el estándar del juego competitivo y ha establecido un modelo a seguir para futuras generaciones de jugadores. Su nombre es sinónimo de excelencia, y su influencia seguirá siendo sentida mucho después de que se retire Faker es, sin lugar a dudas, el epítome de la grandeza en el mundo de los esports, y su historia es un testimonio del poder de la perseverancia, el talento y la pasión
+    </div>
+    """, unsafe_allow_html=True)
+
+    st.image("Faker.jpg")
+
+    video_url = "https://www.youtube.com/watch?v=rVnbFhnn5z4"
+    st.video(video_url)
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>Desde su debut en 2013, Faker ha redefinido lo que significa ser un profesional en League of Legends. Sus habilidades mecánicas y su comprensión profunda del juego le han permitido dominar la mid lane como ningún otro jugador. Ha llevado a su equipo, T1 (anteriormente SK Telecom T1), a múltiples títulos, incluyendo cinco campeonatos mundiales (2013, 2015, 2016, 2023 y 2024), convirtiéndolo en una leyenda viviente.
+    </div>
+    """, unsafe_allow_html=True)
 
 elif opcion == 'Acerca de':
 
